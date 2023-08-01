a6732ba6a1ece7655674d199602de84a2926989e
Andre Rousselet <andre098@hotmail.com>
2023-08-01 18:53:38 -0300

    feat(docs-package): add storybook in /docs package and Button example storie

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,8 @@
 import { styled } from "./styles";
 
-const Button = styled("button", {
+export const Button = styled("button", {
   fontFamily: "$default",
-  backgroundColor: "$ignite500",
+  backgroundColor: "$ignite300",
   borderRadius: "$md",
   padding: "$4",
 });
-
-export function App() {
-  return <Button>Hello World!</Button>;
-}
